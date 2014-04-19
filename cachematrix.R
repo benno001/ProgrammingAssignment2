80162708f16b90315890259126e6ea0491c1108c
benno001 <bjf.dehaan@gmail.com>
2014-04-19 21:39:12 +0200

    Experimental cacheMatrix change

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create cacheable matrix inverse object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initiate object
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  list(set = set, get = get)
 }
 
 
-## Write a short comment describing this function
+## Inverts matrix from makeCacheMatrix.
+## When already cached, retrieve inverted matrix from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$get()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  m <- solve(x, ...)
+  x$set(m)c
+  m
 }
